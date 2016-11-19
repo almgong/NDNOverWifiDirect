65bbc174aee28645ea8742caf3e51d61819c560c
Allen Gong <alm.gong@gmail.com>
2016-11-19 14:45:39 -0800

    add re-tries for register prefix task

--- app/src/main/java/ag/ndn/ndnoverwifidirect/task/RegisterPrefixTask.java
+++ app/src/main/java/ag/ndn/ndnoverwifidirect/task/RegisterPrefixTask.java
@@ -2,32 +2,22 @@ package ag.ndn.ndnoverwifidirect.task;
 
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.intel.jndn.management.Nfdc;
-import com.intel.jndn.management.types.FaceStatus;
-
-import net.named_data.jndn.Data;
 import net.named_data.jndn.Face;
 import net.named_data.jndn.ForwardingFlags;
-import net.named_data.jndn.Interest;
-import net.named_data.jndn.InterestFilter;
 import net.named_data.jndn.Name;
 import net.named_data.jndn.OnInterestCallback;
 import net.named_data.jndn.OnRegisterFailed;
 import net.named_data.jndn.OnRegisterSuccess;
-import net.named_data.jndn.security.KeyChain;
-import net.named_data.jndn.util.Blob;
-
-import java.io.IOException;
-import java.util.List;
 
 import ag.ndn.ndnoverwifidirect.utils.NDNController;
-import ag.ndn.ndnoverwifidirect.utils.NDNOverWifiDirect;
-import ag.ndn.ndnoverwifidirect.utils.NfdcHelper;
 
 /**
- * Provides ability to register one or more prefixes to the specified face.
+ * Provides ability to register one to the specified face.
+ * Given that multiple successive prefix registration calls can fail (NFD timeout),
+ * This task will attempt to register prefixes 5 times or until success. Each
+ * attempt is separated (e.g. 500ms) to increase chance of registration.
+ *
  * Created by allengong on 7/31/16.
  */
 public class RegisterPrefixTask extends AsyncTask<String, Void, Integer> {
@@ -43,6 +33,8 @@ public class RegisterPrefixTask extends AsyncTask<String, Void, Integer> {
     private boolean mStopProcessing, handleForever;
 
     private long processEventsTimer = 500;  // by default, every half second, process events
+    private int attemptNum = 1;             // for re-attempting to register a prefix (in case of timeout)
+    private final int MAX_ATTEMPTS = 5;     // max retries for above
 
     public RegisterPrefixTask(Face f, String prefix, OnInterestCallback cb, boolean forever) {
         this.mFace = f;
@@ -57,24 +49,56 @@ public class RegisterPrefixTask extends AsyncTask<String, Void, Integer> {
     }
     public void setStopProcessing(boolean stop) { this.mStopProcessing = stop; }
 
+    private void onRegisterFailRetry(Name prefix, final ForwardingFlags flags) {
+        try {
+            mFace.registerPrefix(prefix, onInterestCallback,
+                    new OnRegisterFailed() {
+                        @Override
+                        public void onRegisterFailed(Name prefix) {
+                            try {
+                                Thread.sleep(500);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                return;
+                            }
+
+                            if (attemptNum <= MAX_ATTEMPTS) {
+                                Log.e(TAG, "Error on registering prefix task, attempt: " + attemptNum);
+                                attemptNum++;
+                                onRegisterFailRetry(prefix, flags);
+                            }
+                        }
+                    }, new OnRegisterSuccess() {
+                        @Override
+                        public void onRegisterSuccess(Name prefix, long registeredPrefixId) {
+                            Log.d(TAG, "Prefix registered successfully: " + prefixToRegister);
+                        }
+                    },
+                    flags);
+        } catch (Exception e) {
+            Log.e(TAG, "Error in retry " + attemptNum);
+            e.printStackTrace();
+        }
+
+    }
+
     // hardcoded responses for now
     @Override
     protected Integer doInBackground(String... params) {
         try {
-//            // keychain business
-//            KeyChain keyChain = mController.getKeyChain();
-//            mFace.setCommandSigningInfo(keyChain, keyChain.getDefaultCertificateName());
 
             // allow child inherit
-            ForwardingFlags flags = new ForwardingFlags();
+            final ForwardingFlags flags = new ForwardingFlags();
             flags.setChildInherit(true);
 
-           long prefixId = mFace.registerPrefix(new Name(this.prefixToRegister),
-                    this.onInterestCallback,
+            Name prefix = new Name(prefixToRegister);
+
+           mFace.registerPrefix(prefix, onInterestCallback,
                     new OnRegisterFailed() {
                         @Override
                         public void onRegisterFailed(Name prefix) {
-                            Log.e(TAG, "Error on registering face task");
+                            Log.d(TAG, "Failed to register prefix: " + prefix.toString());
+                            onRegisterFailRetry(prefix, flags);
                         }
                     }, new OnRegisterSuccess() {
                         @Override
