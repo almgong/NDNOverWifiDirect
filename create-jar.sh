95381a3aa7e9dfec563a017fe2937b57f24bdae7
Allen Gong <alm.gong@gmail.com>
2016-12-10 22:41:33 -0800

    fix package issue

--- create-jar.sh
+++ create-jar.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+###
+#  MOVE THIS FILE to app/build/intermediates/debug/classes and then execute.
+#
+###
+
 #create a jar called ndn-over-wifid.jar
-BASE_DIR=app/src/main/java/ag/ndn/ndnoverwifidirect
+#BASE_DIR=app/src/main/java/ag/ndn/ndnoverwifidirect
+BASE_DIR=ag/ndn/ndnoverwifidirect
 jar -cfv ndn-over-wifid.jar $BASE_DIR/callback/* $BASE_DIR/model/* $BASE_DIR/runnable/* $BASE_DIR/service/* \
 $BASE_DIR/task/* $BASE_DIR/utils/*
